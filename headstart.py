f0d1ef13dbad5e57a41dc5c63a433439f47c794b
Ziad Ali <ziadalikhalifa@gmail.com>
2020-05-27 07:52:56 +0200

    Added Git creation and first commit

--- headstart.py
+++ headstart.py
@@ -1,19 +1,40 @@
 import subprocess
 
 from getpass import getpass
+from os import listdir, path, mkdir, chdir
 from colorama import Fore, Back, Style
 
 print('\033[39m')
-print("Welcome to " + Fore.CYAN + "HeadStart!")
+print("Welcome to " + Fore.CYAN + "HeadStart" + Style.RESET_ALL + "!")
 print('\033[39m')
 
 repository_name = input(
-    Fore.YELLOW + "What will you be naming this project?: ")
+    Fore.YELLOW + "What will you be naming this project?: " + Style.RESET_ALL).strip()
 
-dir_list = subprocess.run(["ls", "-l", "."], stdout=subprocess.PIPE)
-print(dir_list.stdout)
 
-# subprocess.run([f"mkdir {repository_name}"])
+# Validation for if there is a folder with the same name
+folders_in_directory = [item for item in listdir(".") if path.isdir(item)]
+if repository_name in folders_in_directory:
+    print("A folder already exists in this path with the same name")
+    exit()
+
+# Folder's name was unique
+mkdir(repository_name)
+chdir(repository_name)
+
+# Initialize a git repository locally
+try:
+    subprocess.run(["git", "init"])
+except:
+    print("Please make sure you have Git Installed on this machine.")
+
+# Create a README file
+open("README.md", "w+")
+
+# Make the first commit
+subprocess.run(["git", "add", "."])
+subprocess.run(["git", "commit", "-m", "Initial Commit"])
+
 
 # github_username = input(Fore.MAGENTA + "Please enter your github username: ")
 # github_password = getpass("Please enter your github password: ")
